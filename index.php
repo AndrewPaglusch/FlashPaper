8c1183bceb0abbfa131868cdba855611df1cafcf
Andrew <andrew.paglusch@Andrews-MacBook-Air.local>
2017-05-12 18:59:34 -0500

    Disabled debugging

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-
+    /*
     //DEBUG
     ini_set('display_startup_errors', 1);
     ini_set('display_errors', 1);
     error_reporting(-1);
-
+    */
 
     require_once "functions.php";
 
