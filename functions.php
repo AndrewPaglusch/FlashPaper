bcd08f97f862dfb074eebf7842dfeff2d642be32
root <root@dev01.ny.ny.us.mgmt.bopag.com>
2017-05-12 00:35:34 +0000

    First commit

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php 
+
+    function print_html_form() {
+
+        readfile("main.html");
+    }
+
+    function encrypt_decrypt($action, $password, $string) {
+        $output = false;
+
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = $password;
+        $secret_iv = sha1($password);
+        $key = hash('sha256', $secret_key);
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+        if( $action == 'encrypt' ) {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        }
+        else if( $action == 'decrypt' ){
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+
+        return $output;
+    }
+
+    function write_file($filename, $text) {
+        $file = fopen("secrets/$filename", "w") or die("Unable to write file!");
+        fwrite($file, $text);
+        fclose($file);
+    }
+
+    function read_file($filename) {
+        $file = fopen("secrets/$filename", "r") or die("<h2>This secret can not be found!</h2>");
+        $text = fread($file,filesize("secrets/$filename"));
+        fclose($file);
+
+        return $text;
+    }
+
+    function delete_file($filename) {
+        unlink("secrets/$filename");
+    }
+
+    function random_str() {
+        for ($i = -1; $i <= 32; $i++) {
+        $bytes = openssl_random_pseudo_bytes($i, $cstrong);
+        $out = base64_encode($bytes);
+        }
+
+        return $bytes;
+    }
+
+    function base64_encode_url($input) {
+        return strtr(base64_encode($input), '+/=', '-_$');
+    }
+
+    function base64_decode_url($input) {
+        return base64_decode(strtr($input, '-_$', '+/='));
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+    /*
+    //DEBUG
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    error_reporting(-1);
+    */
+
+    /*
+    GENERATE::
+    Generate password
+    secret text is encrypted with password
+    password hashed
+    hashed password and encrypted text stored in db
+    base64 the password
+    send base64 password to user as URL
+
+    RETREIVE::
+    Get Base64 portion of URL
+    Un-Base64 the URL to get the random password
+    sha1 the password
+    look up the sha1 of the password in the db
+    get the associated ciphertext and decrypt with the un-base64'd version of the password
+    return decrypted ciphertext to user
+    remove entry from database
+    */
+
+    require_once "functions.php";
+
+    if (isset($_GET['k'])) {
+        $base_pass = $_GET['k'];
+        $password = base64_decode_url($base_pass);
+        $sha_pass = sha1($password);
+        $enc_text = read_file($sha_pass);
+        $dec_text = encrypt_decrypt("decrypt", $password, $enc_text);
+        
+        /* DEBUG
+        echo "<b>Given Password in Base64 (URL Safe) Form:</b> " . $base_pass . "<br />";
+        echo "<b>Decoded Version of Given Password:</b> " . $password . "<br />";
+        echo "<b>Hash of Given Password:</b> " . $sha_pass . "<br />";
+
+        echo "Reading secret from file " . $sha_pass . "<br />";
+        echo "Encrypted Secret Associated with SHA1 of Given Password: " . $enc_text . "<br />";
+        echo "Decrypted Secret Associated with SHA1 of Given Password: " . $dec_text . "<br />";
+	    */
+
+        echo htmlentities($dec_text);
+        echo "<hr /><br />";
+
+        delete_file($sha_pass);
+        echo "This secret has been destroyed" . "<br />";
+
+    } elseif (isset($_POST['submit'])) {
+        $rand_pass = random_str();
+        $enc_text = encrypt_decrypt("encrypt", $rand_pass, $_POST['secret']);
+        $dec_text = encrypt_decrypt("decrypt", $rand_pass, $enc_text);
+        $sha_pass = sha1($rand_pass);
+        $base_pass = base64_encode_url($rand_pass);
+      
+        write_file($sha_pass, $enc_text);
+        
+        /* DEBUG
+        echo "<b>Submitted Text:</b> " . $_POST['secret'] . "<br />";
+        echo "<b>Random Pasword:</b> " . $rand_pass . "<br />";
+        echo "<b>Hash of Random Password:</b> " . $sha_pass . "<br />";
+        echo "<b>Base64 (URL Safe) of Random Password:</b> " . $base_pass . "<br />";
+        echo "<hr />";
+        echo "<b>Encrypted text:</b> " . $enc_text . "<br />";
+        echo "<b>Decrypted text:</b> " . $dec_text . "<br />";
+        echo "<hr />";
+
+        echo "<b>Here's how the database entry for this will look:</b>" . "<br /><br />";
+        echo "<table style='width: 90%'><tr><td><u>KEY (SHA1 of Random Password)</u></td><td><u>VALUE (Encrypted Secret Text)</u></td></tr><tr><td><b>$sha_pass</b></td><td>$enc_text</td></tr></table>";
+        echo "<hr />";
+
+        echo "<b>Here's how the URL you'll share will look:</b>" . "<br />";
+        echo "https://password.paglusch.com/?k=" . $base_pass;
+        echo "<br /><br /><br />";
+	    */
+    	
+	//echo "http://password.paglusch.com/?k=" . $base_pass;
+    echo $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . "/?k=" . $base_pass;
+
+	} else {
+        print_html_form();
+    }
+
+?>
