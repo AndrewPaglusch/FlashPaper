bef29c1f00c84776812efdb035de6e849677dea2
Andrew <andrew.paglusch@Andrews-MacBook-Air.local>
2017-05-17 16:05:59 -0500

    Turned on error display

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
-    /*
     //DEBUG
     ini_set('display_startup_errors', 1);
     ini_set('display_errors', 1);
     error_reporting(-1);
-    */
+
 
     require_once "includes/functions.php";
 
