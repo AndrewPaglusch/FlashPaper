6cb1ad89acbd314d668f1fdb9df6c29a4f8f9c5c
Andrew Paglusch <AndrewPaglusch@users.noreply.github.com>
2017-05-18 09:43:15 -0500

    Update index.php
    Removed debugging

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-    //DEBUG
+    /* //DEBUG
     ini_set('display_startup_errors', 1);
     ini_set('display_errors', 1);
-    error_reporting(-1);
+    error_reporting(-1); */
 
 
     require_once "includes/functions.php";
