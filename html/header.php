e79bb46784614c2a71d7f80a6fd877ac200e6a86
Barry Smith <barry.smithjr@gmail.com>
2018-05-10 13:37:07 -0500

    Add Boostrap 4

--- html/header.php
+++ html/header.php
@@ -2,6 +2,11 @@
 <!DOCTYPE html>
     <head>
         <title>FlashPaper :: Self-Destructing Message</title>
+		<!-- Meta tags -->
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <style>
             #form-div {
                 background-color:rgba(72,72,72,0.1);
