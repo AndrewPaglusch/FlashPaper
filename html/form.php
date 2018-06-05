e4e543f9f65e76e50b261af9881e5f9245ed0a2c
Barry Smith <barry.smithjr@gmail.com>
2018-06-04 19:01:43 -0500

    Fix spacing 2

--- html/form.php
+++ html/form.php
@@ -12,20 +12,19 @@
                             <textarea class="form-control" id="secret" name="secret" placeholder="Secret text..."><?php echo $template_text ?></textarea>
                         </div>
                     </div>
-					<div class="clearfix visible-xs"></div>
+                    <div class="clearfix visible-xs"></div>
                     <div class="form-group row float-right" style='padding-top: 3%'>
-                      <!-- <label class="col-4 col-form-label" for="select">Select Template</label> -->
-					<div class="w-100"></div>
-					<div class="col">
+                    <div class="w-100"></div>
+                    <div class="col">
                          <select id="select" name="select" class="custom-select" onChange="window.location.href=this.value">
-							<option value="" selected disabled hidden>-- Select Template</option>
+                            <option value="" selected disabled hidden>-- Select Template</option>
                             <option value="/">No Template</option>
                             <option value="?t=cc">Credit Card</option>
                             <option value="?t=creds">Credentials</option>
                          </select>
                        </div>
                      </div> 
-					<div class="w-100"></div>
+                     <div class="w-100"></div>
                      <div class="form-group row float-left" style='padding-top: 3%'>
                         <div class="col">
                            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
