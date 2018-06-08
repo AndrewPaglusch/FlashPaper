4604acbcc45e6034a1b18329fa6c2c5c0603cc0a
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2018-06-08 19:07:35 +0000

    fix indents. squash warning about GET var "accept" being set

--- includes/functions.php
+++ includes/functions.php
@@ -8,13 +8,13 @@
     $static_key = 'FAg&Se(YO3h!Ib?K2W^>Gv[n?h)w)!y>';
 
     function encrypt_decrypt($encrypt, $key, $string) {
-        $iv = substr(hash('sha256', $key), 0, 16); #sha256 sha1 of key and get first 16 bytes
+        $iv = substr(hash('sha256', $key), 0, 16); #get first 16 bytes of sha256(key) for IV
+        $enc_method = 'AES-256-CBC';
 
         if( $encrypt == true) {
-            return openssl_encrypt($string, "AES-256-CBC", $key, 0, $iv);
-        }
-        else {
-            return openssl_decrypt($string, "AES-256-CBC", $key, 0, $iv);
+            return openssl_encrypt($string, $enc_method, $key, 0, $iv);
+        } else {
+            return openssl_decrypt($string, $enc_method, $key, 0, $iv);
         }
     }
 
@@ -57,53 +57,53 @@
     }
 
     function store_secret($text) {
-      global $bcrypt_options, $static_key;
-
-      #generate random key
-      $rand_key = random_str();
-
-      #base64 encode the key (for URL)
-      $base_key = base64_encode_mod($rand_key);
-
-      #encrypt text with random key
-      $enc_text = encrypt_decrypt(true, $rand_key, $text);
-
-      #encrypt text with static key
-      $enc_text = encrypt_decrypt(true, $static_key, $enc_text);
-
-      #generate hash of key & base64 it
-      $filename = base64_encode_mod(password_hash($rand_key, PASSWORD_BCRYPT, $bcrypt_options));
-
-      #write encrypted text to disk. filename is hash of key
-      write_file("secrets/" . $filename, $enc_text);
-
-      #return base64 of key
-      return $base_key;
+        global $bcrypt_options, $static_key;
+        
+        #generate random key
+        $rand_key = random_str();
+        
+        #base64 encode the key (for URL)
+        $base_key = base64_encode_mod($rand_key);
+        
+        #encrypt text with random key
+        $enc_text = encrypt_decrypt(true, $rand_key, $text);
+        
+        #encrypt text with static key
+        $enc_text = encrypt_decrypt(true, $static_key, $enc_text);
+        
+        #generate hash of key & base64 it
+        $filename = base64_encode_mod(password_hash($rand_key, PASSWORD_BCRYPT, $bcrypt_options));
+        
+        #write encrypted text to disk. filename is hash of key
+        write_file("secrets/" . $filename, $enc_text);
+        
+        #return base64 of key
+        return $base_key;
     }
 
     function retrieve_secret($key) {
-      global $bcrypt_options, $static_key;
-      
-      #decode key from url with modified base64
-      $key = base64_decode_mod($key);
-
-      #generate hash of key & base64 it
-      $filename = base64_encode_mod(password_hash($key, PASSWORD_BCRYPT, $bcrypt_options));
-
-      #read file that is named same as the hash of key
-      $enc_text = read_file("secrets/" . $filename);
-
-      #decrypt contents of file with the static key
-      $dec_text = encrypt_decrypt(false, $static_key, $enc_text);
-
-      #decrypt contents of file with the base64 decoded key
-      $dec_text = encrypt_decrypt(false, $key, $dec_text);
-
-      #delete the file from disk
-      delete_file("secrets/" . $filename);
-
-      #return decrypted text
-      return $dec_text;
+        global $bcrypt_options, $static_key;
+        
+        #decode key from url with modified base64
+        $key = base64_decode_mod($key);
+        
+        #generate hash of key & base64 it
+        $filename = base64_encode_mod(password_hash($key, PASSWORD_BCRYPT, $bcrypt_options));
+        
+        #read file that is named same as the hash of key
+        $enc_text = read_file("secrets/" . $filename);
+        
+        #decrypt contents of file with the static key
+        $dec_text = encrypt_decrypt(false, $static_key, $enc_text);
+        
+        #decrypt contents of file with the base64 decoded key
+        $dec_text = encrypt_decrypt(false, $key, $dec_text);
+        
+        #delete the file from disk
+        delete_file("secrets/" . $filename);
+        
+        #return decrypted text
+        return $dec_text;
     }
 
 ?>
