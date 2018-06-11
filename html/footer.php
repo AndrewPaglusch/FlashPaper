059211af4dded13e977a362fbc9bc54a504e5295
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2018-06-11 17:11:14 +0000

    added footer with copyright and github link

--- html/footer.php
+++ html/footer.php
@@ -1,2 +1,7 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
+	<footer style="position: absolute; bottom: 0; width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;">
+		<div class="container">
+			<span class="text-muted small">Copyright (c) 2017 Andrew Paglusch | <a href="https://github.com/AndrewPaglusch/FlashPaper">Github</a></span>
+		</div>
+	</footer>
 </html>
