cfe24c906906cc7d5618be0028687c13b7f2c472
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2018-06-12 23:56:47 +0000

    sqlite now used as storage engine. random salt now used for each secret

--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,8 @@
 
 	defined('_DIRECT_ACCESS_CHECK') or exit();
 
-	$bcrypt_options = ['cost' => 11, 'salt' => base64_decode('6bDvAudGJKkTweOWIlxVbTWjmj/8kDL9giAPkGDN7qhCi+jM50eS/2ijplJ1jf7T1+ZF1pUmtw2xlxsb0hlr3w==')];
+	$bcrypt_cost = 11;
+
 	$static_key = base64_decode('tGOrlqr/97qxQwby+uwsbReOLxTLgrMntDKX/Uj4LMy6YSSQ9Xr4DjMgKVhnUT2pZ/YFJUo/qE/xBMD5dZBF9ZBZTnPNz+Pnez1OazpoEAy2M3vE7N/kQ4tP7kA98jf+NCKqi8MLJ6hQPMPXFuciIQsKUNWc6clJ+q5GwJAxikyxy8VCnDgOEoV0u6GpVB7syrB1OlvORAWsB7wqkq2XmiZnRVJsnz/td6kBhnwPE5F7ghlncZcyXjuL86M/bFlrqtm6pH6mVLWIAeRFdH7jVdgB/ihVsnaxXXkHnY9AZEzmuk19r+IiRHIv+ft299t//ddFM5lGduwqKCJ8tfpWFQ==');
 
 	function encrypt_decrypt($encrypt, $key, $iv, $string) {
@@ -13,48 +14,54 @@
 		}
 	}
 
-	function write_file($filename, $text, $randPrefix = false) {
-		if ( $randPrefix ) {
-			$prefix = substr(str_shuffle(implode(array_merge(range('A','Z'), range('a','z'), range(0,9)))), 0, 20);
-			$filename = dirname($filename) . "/" . $prefix . "---" . basename($filename);
-		}
-		if ($fp = fopen($filename, "w")) {
-			fwrite($fp, $text);
-			fclose($fp);
-		} else {
-			throw new Exception('Unable to write secret!');
-		}
+	function connect($databaseName) {
+		$db = new PDO("sqlite:{$databaseName}");
+		$db->exec('CREATE TABLE IF NOT EXISTS "secrets" ("hash" TEXT PRIMARY KEY, "iv" TEXT, "secret" TEXT)');
+		$db->exec('CREATE TABLE IF NOT EXISTS "salts" ("id" TEXT PRIMARY KEY, "salt" TEXT)');
+		return $db;
 	}
 
-	function read_file($filename, $randPrefix = false) {
-		if ( $randPrefix ) {
-			$results = glob(dirname($filename) . '/*--' . basename($filename));
-			if ( count($results) != 1 ) {
-				throw new Exception('This secret can not be found!');
-			} else {
-				$filename = $results[0];
-			}
-		}
-		if ( file_exists($filename) && ($fp = fopen($filename, "rb")) !== false ) {
-			$str = stream_get_contents($fp);
-			fclose($fp);
-			return $str;
-		} else {
-			throw new Exception('This secret can not be found!');
-		}
+	function writeSecret($db, $hash, $iv, $secret) {
+		$statement = $db->prepare('INSERT INTO "secrets" ("hash", "iv", "secret") VALUES (:hash, :iv, :secret)');
+		$statement->bindValue(':hash', $hash);
+		$statement->bindValue(':iv', $iv);
+		$statement->bindValue(':secret', $secret);
+		$statement->execute();
 	}
 
-	function delete_file($filename, $randPrefix) {
-		if ( $randPrefix ) {
-			$results = glob(dirname($filename) . '/*--' . basename($filename));
-			if ( count($results) != 1 ) {
-				throw new Exception('Failed to delete secret!');
-			} else {
-				unlink($results[0]);
-			}
-		} else {
-			unlink($filename);
-		}
+	function writeSalt($db, $saltid, $salt) {
+		$statement = $db->prepare('INSERT INTO "salts" ("id", "salt") VALUES (:id, :salt)');
+		$statement->bindValue(':id', $saltid);
+		$statement->bindValue(':salt', $salt);
+		$statement->execute();
+	}
+
+	function readSecret($db, $hash) {
+		$statement = $db->prepare('SELECT * FROM "secrets" WHERE hash = :hash LIMIT 1');
+		$statement->bindValue(':hash', $hash);
+		$statement->execute();
+		$result = $statement->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
+
+	function readSalt($db, $saltid) {
+		$statement = $db->prepare('SELECT * FROM "salts" WHERE id = :saltid LIMIT 1');
+		$statement->bindValue(':saltid', $saltid);
+		$statement->execute();
+		$result = $statement->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
+
+	function deleteSecret($db, $hash) {
+		$statement = $db->prepare('DELETE FROM "secrets" WHERE hash = :hash');
+		$statement->bindValue(':hash', $hash);
+		$statement->execute();
+	}
+
+	function deleteSalt($db, $saltid) {
+		$statement = $db->prepare('DELETE FROM "salts" WHERE id = :saltid');
+		$statement->bindValue(':saltid', $saltid);
+		$statement->execute();
 	}
 
 	function random_str($len) {
@@ -72,65 +79,114 @@
 		return base64_decode(strtr($input, '-_$', '+/='));
 	}
 
-	function store_secret($text) {
-		global $bcrypt_options, $static_key;
+	function store_secret($secret) {
+		global $bcrypt_cost, $static_key;
 
-		#generate random key
-		$rand_key = random_str(32);
+		#connect to sqlite db
+		$db = connect('secrets.sqlite');
 
-		#generate random iv
+		#generate random key, iv, salt, and saltid
+		$key = random_str(32);
 		$iv = random_str(16);
+		$salt = random_str(64);
+		$saltid = random_str(16);
 
-		#base64 encode the key (for URL)
-		$base_key = base64_encode_mod($iv . $rand_key);
+		#generate hash of key
+		$secret_hash = password_hash($key, PASSWORD_BCRYPT, ['cost' => $bcrypt_cost, 'salt' => $salt]);
 
-		#encrypt text with random key
-		$enc_text = encrypt_decrypt(true, $rand_key, $iv, $text);
+		#encrypt text with key and then static key
+		$secret = encrypt_decrypt(true, $key, $iv, $secret);
+		$secret = encrypt_decrypt(true, $static_key, $iv, $secret);
 
-		#encrypt text with static key
-		$enc_text = encrypt_decrypt(true, $static_key, $iv, $enc_text);
+		#generate k value for url
+		$k = base64_encode_mod($saltid . $key);
 
-		#generate hash of key & base64 it
-		$filename = base64_encode_mod(password_hash($iv . $rand_key, PASSWORD_BCRYPT, $bcrypt_options));
+		#base64 encode the secret_hash, iv, salt, saltid, and secret for db storage
+		$secret_hash = base64_encode_mod($secret_hash);
+		$iv = base64_encode_mod($iv);
+		$salt = base64_encode_mod($salt);
+		$saltid = base64_encode_mod($saltid);
+		$secret = base64_encode_mod($secret);
 
-		#write encrypted text to disk. filename is hash of key
-		write_file("secrets/" . $filename, $enc_text, true);
+		#write secret_hash, iv, and secret to database
+		writeSecret($db, $secret_hash, $iv, $secret);
 
-		#return base64 of key
-		return $base_key;
+		#write saltid and salt to database
+		writeSalt($db, $saltid, $salt);
+
+		#close db
+		$db = null;
+
+		#base64 encode the saltID + key (for URL)
+		return $k;
 	}
 
 	function retrieve_secret($k) {
-		global $bcrypt_options, $static_key;
+		global $bcrypt_cost, $static_key;
 
-		#validate length of key - must be 48 chars (iv = 16, key = 32)
+		#connect to sqlite db
+		$db = connect('secrets.sqlite');
+
+		#validate length of k - must be 48 chars (saltid = 16, key = 32)
 		if ( strlen(base64_decode_mod($k)) != 48 ) {
 			throw new Exception('This secret can not be found!');
 		}
 
-		#decode key from url with modified base64
-		$key = substr(base64_decode_mod($k), -32);
+		#base64 decode k
+		$k = base64_decode_mod($k);
 
-		#decode iv from url
-		$iv = substr(base64_decode_mod($k), 0, 16);
+		#extract saltid from k and base64 encode it
+		$saltid = substr($k, 0, 16);
+		$saltid = base64_encode_mod($saltid);
 
-		#generate hash of key & base64 it
-		$filename = base64_encode_mod(password_hash($iv . $key, PASSWORD_BCRYPT, $bcrypt_options));
+		#look up salt with saltid
+		$saltResult = readSalt($db, $saltid);
 
-		#read file that is named same as the hash of key
-		$enc_text = read_file("secrets/" . $filename, true);
+		#throw exception if query failed
+		if ( ! $saltResult ) {
+			throw new Exception('This secret can not be found!');
+		}
 
-		#decrypt contents of file with the static key
-		$dec_text = encrypt_decrypt(false, $static_key, $iv, $enc_text);
+		#get salt from query results and base64 decode it
+		$salt = $saltResult['salt'];
+		$salt = base64_decode_mod($salt);
 
-		#decrypt contents of file with the base64 decoded key
-		$dec_text = encrypt_decrypt(false, $key, $iv, $dec_text);
+		#extract key from k
+		$key = substr($k, -32);
 
-		#delete the file from disk
-		delete_file("secrets/" . $filename, true);
+		#generate hash of iv + key & base64 encode it
+		$secret_hash = password_hash($key, PASSWORD_BCRYPT, ['cost' => $bcrypt_cost, 'salt' => $salt]);
+		$secret_hash = base64_encode_mod($secret_hash);
+
+		#read secret, hash, and iv from db
+		$db_result = readSecret($db, $secret_hash);
+
+		#throw exception if query failed
+		if ( ! $db_result ) {
+			throw new Exception('This secret can not be found!');
+		}
+
+		#get iv from query results and base64 decode it
+		$iv = $db_result['iv'];
+		$iv = base64_decode_mod($iv);
+
+		#get secret from query results and base64 decode it
+		$secret = $db_result['secret'];
+		$secret = base64_decode_mod($secret);
+
+		#decrypt secret with the static key, and then with url key
+		$secret = encrypt_decrypt(false, $static_key, $iv, $secret);
+		$secret = encrypt_decrypt(false, $key, $iv, $secret);
+
+		#delete secret and salt from db
+		deleteSecret($db, $secret_hash);
+		deleteSalt($db, $saltid);
+
+		#close db
+		$db = null;
 
 		#return decrypted text
-		return $dec_text;
+		return $secret;
 	}
 
 ?>
