11bbcb923db8c27c99d40a00093a44acb89b04a3
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2018-06-13 20:17:07 +0000

    removed salt table. secrets now referred to by 8-byte id. id included in url. no more static cost or self-gen salts for bcrpyt. password_verify used to verify bcrypt hashes

--- includes/functions.php
+++ includes/functions.php
@@ -2,9 +2,7 @@
 
 	defined('_DIRECT_ACCESS_CHECK') or exit();
 
-	$bcrypt_cost = 11;
-
-	$static_key = base64_decode('tGOrlqr/97qxQwby+uwsbReOLxTLgrMntDKX/Uj4LMy6YSSQ9Xr4DjMgKVhnUT2pZ/YFJUo/qE/xBMD5dZBF9ZBZTnPNz+Pnez1OazpoEAy2M3vE7N/kQ4tP7kA98jf+NCKqi8MLJ6hQPMPXFuciIQsKUNWc6clJ+q5GwJAxikyxy8VCnDgOEoV0u6GpVB7syrB1OlvORAWsB7wqkq2XmiZnRVJsnz/td6kBhnwPE5F7ghlncZcyXjuL86M/bFlrqtm6pH6mVLWIAeRFdH7jVdgB/ihVsnaxXXkHnY9AZEzmuk19r+IiRHIv+ft299t//ddFM5lGduwqKCJ8tfpWFQ==');
+	$staticKey = base64_decode('tGOrlqr/97qxQwby+uwsbReOLxTLgrMntDKX/Uj4LMy6YSSQ9Xr4DjMgKVhnUT2pZ/YFJUo/qE/xBMD5dZBF9ZBZTnPNz+Pnez1OazpoEAy2M3vE7N/kQ4tP7kA98jf+NCKqi8MLJ6hQPMPXFuciIQsKUNWc6clJ+q5GwJAxikyxy8VCnDgOEoV0u6GpVB7syrB1OlvORAWsB7wqkq2XmiZnRVJsnz/td6kBhnwPE5F7ghlncZcyXjuL86M/bFlrqtm6pH6mVLWIAeRFdH7jVdgB/ihVsnaxXXkHnY9AZEzmuk19r+IiRHIv+ft299t//ddFM5lGduwqKCJ8tfpWFQ==');
 
 	function encrypt_decrypt($encrypt, $key, $iv, $string) {
 		if( $encrypt == true) {
@@ -16,157 +14,124 @@
 
 	function connect($databaseName) {
 		$db = new PDO("sqlite:{$databaseName}");
-		$db->exec('CREATE TABLE IF NOT EXISTS "secrets" ("hash" TEXT PRIMARY KEY, "iv" TEXT, "secret" TEXT)');
-		$db->exec('CREATE TABLE IF NOT EXISTS "salts" ("id" TEXT PRIMARY KEY, "salt" TEXT)');
+		$db->exec('CREATE TABLE IF NOT EXISTS "secrets" ("id" TEXT PRIMARY KEY, "iv" TEXT, "hash" TEXT, "secret" TEXT)');
 		return $db;
 	}
 
-	function writeSecret($db, $hash, $iv, $secret) {
-		$statement = $db->prepare('INSERT INTO "secrets" ("hash", "iv", "secret") VALUES (:hash, :iv, :secret)');
-		$statement->bindValue(':hash', $hash);
+	function writeSecret($db, $id, $iv, $hash, $secret) {
+		$statement = $db->prepare('INSERT INTO "secrets" ("id", "iv", "hash", "secret") VALUES (:id, :iv, :hash, :secret)');
+		$statement->bindValue(':id', $id);
 		$statement->bindValue(':iv', $iv);
+		$statement->bindValue(':hash', $hash);
 		$statement->bindValue(':secret', $secret);
 		$statement->execute();
 	}
 
-	function writeSalt($db, $saltid, $salt) {
-		$statement = $db->prepare('INSERT INTO "salts" ("id", "salt") VALUES (:id, :salt)');
-		$statement->bindValue(':id', $saltid);
-		$statement->bindValue(':salt', $salt);
-		$statement->execute();
-	}
-
-	function readRow($db, $table, $key, $value) {
-		$statement = $db->prepare('SELECT * FROM "' . $table . '" WHERE ' . $key . ' = :value LIMIT 1');
-		$statement->bindValue(':value', $value);
+	function readSecret($db, $id) {
+		$statement = $db->prepare('SELECT * FROM "secrets" WHERE id = :id LIMIT 1');
+		$statement->bindValue(':id', $id);
 		$statement->execute();
 		$result = $statement->fetch(PDO::FETCH_ASSOC);
 		return $result;
 	}
 
-	function deleteRow($db, $table, $key, $value) {
-		$statement = $db->prepare('DELETE FROM "' . $table . '" WHERE ' . $key . ' = :value');
-		$statement->bindValue(':value', $value);
+	function deleteSecret($db, $id) {
+		$statement = $db->prepare('DELETE FROM "secrets" WHERE id = :id');
+		$statement->bindValue(':id', $id);
 		$statement->execute();
 	}
 
-	function random_str($len) {
-		for ($i = -1; $i <= $len; $i++) {
+	function random_str($byteLen) {
+		for ($i = -1; $i <= $byteLen; $i++) {
 			$bytes = openssl_random_pseudo_bytes($i, $cstrong);
 		}
 		return $bytes;
 	}
 
 	function base64_encode_mod($input) {
-		return strtr(base64_encode($input), '+/=', '-_$');
+		return strtr(base64_encode($input), '+/=', '-_#');
 	}
 
 	function base64_decode_mod($input) {
-		return base64_decode(strtr($input, '-_$', '+/='));
+		return base64_decode(strtr($input, '-_#', '+/='));
 	}
 
 	function store_secret($secret) {
-		global $bcrypt_cost, $static_key;
+		global $staticKey;
 
 		#connect to sqlite db
 		$db = connect('secrets.sqlite');
 
-		#generate random key, iv, salt, and saltid
-		$key = random_str(32);
+		#generate random id, iv, key
+		$id = random_str(8);
 		$iv = random_str(16);
-		$salt = random_str(8);
-		$saltid = random_str(16);
+		$key = random_str(32);
 
-		#generate hash of key
-		$secret_hash = password_hash($key, PASSWORD_BCRYPT, ['cost' => $bcrypt_cost, 'salt' => $salt]);
+		#generate k value for url (id + key)
+		$k = base64_encode_mod($id . $key);
+
+		#generate hash of id + key
+		$hash = password_hash($id . $key, PASSWORD_BCRYPT);
 
 		#encrypt text with key and then static key
 		$secret = encrypt_decrypt(true, $key, $iv, $secret);
-		$secret = encrypt_decrypt(true, $static_key, $iv, $secret);
+		$secret = encrypt_decrypt(true, $staticKey, $iv, $secret);
 
-		#generate k value for url
-		$k = base64_encode_mod($saltid . $key);
-
-		#base64 encode the secret_hash, iv, salt, saltid, and secret for db storage
-		$secret_hash = base64_encode_mod($secret_hash);
+		#base64 encode the id, iv, and secret for db storage
+		$id = base64_encode_mod($id);
 		$iv = base64_encode_mod($iv);
-		$salt = base64_encode_mod($salt);
-		$saltid = base64_encode_mod($saltid);
 		$secret = base64_encode_mod($secret);
 
 		#write secret_hash, iv, and secret to database
-		writeSecret($db, $secret_hash, $iv, $secret);
-
-		#write saltid and salt to database
-		writeSalt($db, $saltid, $salt);
+		writeSecret($db, $id, $iv, $hash, $secret);
 
 		#close db
 		$db = null;
 
-		#base64 encode the saltID + key (for URL)
+		#return base64(id + key)
 		return $k;
 	}
 
 	function retrieve_secret($k) {
-		global $bcrypt_cost, $static_key;
+		global $staticKey;
 
 		#connect to sqlite db
 		$db = connect('secrets.sqlite');
 
-		#validate length of k - must be 48 chars (saltid = 16, key = 32)
-		if ( strlen(base64_decode_mod($k)) != 48 ) {
+		#validate length of k - must be 40 chars (id = 8, key = 32)
+		if ( strlen(base64_decode_mod($k)) != 40 ) {
 			throw new Exception('This secret can not be found!');
 		}
 
-		#base64 decode k
+		#extract key and id from k. base64 encode id
 		$k = base64_decode_mod($k);
-
-		#extract saltid from k and base64 encode it
-		$saltid = substr($k, 0, 16);
-		$saltid = base64_encode_mod($saltid);
-
-		#look up salt with saltid
-		$saltResult = readRow($db, 'salts', 'id', $saltid);
-
-		#throw exception if query failed
-		if ( ! $saltResult ) {
-			throw new Exception('This secret can not be found!');
-		}
-
-		#get salt from query results and base64 decode it
-		$salt = $saltResult['salt'];
-		$salt = base64_decode_mod($salt);
-
-		#extract key from k
 		$key = substr($k, -32);
+		$id = substr($k, 0, 8);
+		$idBase64 = base64_encode_mod($id);
 
-		#generate hash of iv + key & base64 encode it
-		$secret_hash = password_hash($key, PASSWORD_BCRYPT, ['cost' => $bcrypt_cost, 'salt' => $salt]);
-		$secret_hash = base64_encode_mod($secret_hash);
-
-		#read secret, hash, and iv from db
-		$db_result = readRow($db, 'secrets', 'hash', $secret_hash);
+		#look up secret by id
+		$secretQuery = readSecret($db, $idBase64);
 
 		#throw exception if query failed
-		if ( ! $db_result ) {
+		if ( ! $secretQuery ) {
 			throw new Exception('This secret can not be found!');
 		}
 
-		#get iv from query results and base64 decode it
-		$iv = $db_result['iv'];
-		$iv = base64_decode_mod($iv);
+		$iv = base64_decode_mod($secretQuery['iv']);
+		$hash = $secretQuery['hash'];
+		$secret = base64_decode_mod($secretQuery['secret']);
 
-		#get secret from query results and base64 decode it
-		$secret = $db_result['secret'];
-		$secret = base64_decode_mod($secret);
+		#verify hash from DB equals hash of id + key from URL
+		if ( ! password_verify($id . $key, $hash)) {
+			throw new Exception('This secret can not be found!');
+		}
 
 		#decrypt secret with the static key, and then with url key
-		$secret = encrypt_decrypt(false, $static_key, $iv, $secret);
+		$secret = encrypt_decrypt(false, $staticKey, $iv, $secret);
 		$secret = encrypt_decrypt(false, $key, $iv, $secret);
 
-		#delete secret and salt from db
-		deleteRow($db, 'secrets', 'hash', $secret_hash);
-		deleteRow($db, 'salts', 'id', $saltid);
+		#delete secret from db
+		deleteSecret($db, $idBase64);
 
 		#close db
 		$db = null;
