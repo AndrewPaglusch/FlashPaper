fe443f8ab1b13f2db676af2af6fda1abdc010bfb
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2019-05-02 21:20:52 +0000

    display warning when not accessed over https

--- html/header.php
+++ html/header.php
@@ -69,3 +69,8 @@
 		</style>
 	</head>
 	<body onUnload="document.getElementById('secret').value = ''">
+	<?php
+		if ($_SERVER['REQUEST_SCHEME'] != 'https') {
+			echo '<div style="padding-top: 1%" class="container"><div class="alert alert-danger"><strong>Danger!</strong> This site is not being accessed over an encrypted connection. Do NOT input any sensitive information!</div></div>';
+		}
+	?>
