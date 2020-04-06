c51dd54919d9d5cabc7ad477e51e99e480bfcd96
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2020-04-06 19:15:03 +0000

    pre-flight checks

---
+++ includes/sanitycheck.php
@@ -0,0 +1,41 @@
+<?php
+	defined('_DIRECT_ACCESS_CHECK') or exit();
+
+	function sanity_check() {
+		$errors = [];
+
+		# make sure data dir is writable. doesn't account for missing executable bit
+		if ( ! is_writable("./" . constant('DATA_DIR')) ) {
+			$errors[] = 'Data directory is not writable';
+		}
+
+		# make sure sqlite is installed
+		if ( ! in_array("sqlite", PDO::getAvailableDrivers()) ) {
+			$errors[] = 'PHP SQLite module is not installed';
+		}
+
+		return $errors;
+	}
+
+	# perform pre-flight check before we do anything
+	$check_results = sanity_check();
+
+	if ( count($check_results) > 0 ) {
+		include('html/header.php');
+		foreach ($check_results as $issue) { $error_message .= "<li>{$issue}</li>"; }
+
+		echo <<<EOS
+		<div style='padding-top: 1%' class='container'>
+			<div class='alert alert-danger'>
+				<h4 class='alert-heading'>FlashPaper Configuration Issues</h4>
+				<p>The following issues are preventing FlashPaper from loading.</p>
+				<hr>
+				<ul>{$error_message}</ul>
+			</div>
+		</div>
+
+EOS;
+		include('html/footer.php');
+		die();
+	}
+?>
--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 	define('DATA_DIR', 'data');
 
 	define('_DIRECT_ACCESS_CHECK', 1);
+
+	# check everything before we proceed
+	require_once("includes/sanitycheck.php");
+
 	require_once "includes/functions.php";
 
-	include('html/header.php');
+	require_once('html/header.php');
 
 	if (isset($_GET['k'])) {
 		#**User is trying to view a secret**
-		include('html/confirm.php');
+		require_once('html/confirm.php');
 	} elseif (isset($_POST['k'])) {
 		#**User confirmed viewing the secret**
 		try {
@@ -20,10 +24,10 @@
 			$message_title = "Self-Destructing Message";
 			$message_subtitle = "This message has been destroyed";
 
-			include('html/message.php');
+			require_once('html/message.php');
 		} catch (Exception $e) {
 			$error_message = $e->getMessage();
-			include('html/error.php');
+			require_once('html/error.php');
 		}
 	} elseif (isset($_POST['submit'])) {
 		#**User just submitted a secret. Show them the generated URL**
@@ -53,10 +57,10 @@
 			$message_title = "Self-Destructing URL";
 			$message_subtitle = "";
 
-			include('html/message.php');
+			require_once('html/message.php');
 		} catch (Exception $e) {
 			$error_message = $e->getMessage();
-			include('html/error.php');
+			require_once('html/error.php');
 		}
 	} else {
 		#**User is loading the main page**
@@ -72,12 +76,12 @@
 			$message_title = "Self-Destructing Message";
 			$message_subtitle = "";
 
-			include('html/form.php');
+			require_once('html/form.php');
 		} catch (Exception $e) {
 			$error_message = "Template can not be found!";
-			include('html/error.php');
+			require_once('html/error.php');
 	   	}
 	}
 
-	include('html/footer.php');
+	require_once('html/footer.php');
 ?>
