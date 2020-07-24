78e7759b0c39f3b14b8cdead86662cb9a9a2b284
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2020-07-24 03:16:02 +0000

    remove unused function

--- includes/functions.php
+++ includes/functions.php
@@ -218,17 +218,4 @@
 		return $secret;
 	}
 
-	if ( ! function_exists('env') ) {
-		function env($key, $default = null)
-		{
-			$value = getenv($key);
-  
-			if ($value === false) {
-				return $default;
-			}
-  
-			return $value;
-		}
-	}
-
 ?>
