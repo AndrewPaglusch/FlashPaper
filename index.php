65f877074e75279d9e74c9c843a973b5d9a04b29
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2020-07-25 01:38:35 +0000

    break apart into functions

--- index.php
+++ index.php
@@ -1,83 +1,88 @@
 <?php
 	define('_DIRECT_ACCESS_CHECK', 1);
+	require_once("includes/sanitycheck.php"); # check everything before we proceed
+	require_once("settings.php"); # load settings
+	require_once("includes/functions.php"); # load functions
+	require_once('html/header.php'); # display header
 
-	# check everything before we proceed
-	require_once("includes/sanitycheck.php");
-
-	# load settings
-	require_once("settings.php");
-
-	# load functions
-	require_once("includes/functions.php");
-
-	# display header
-	require_once('html/header.php');
 
 	if (isset($_GET['k'])) {
-		#**User is trying to view a secret**
-		require_once('html/confirm.php');
+		try {
+			confirm_display_secret(); # ask confirmation before showing secret
+		} catch (Exception $e) { display_error($e); }
 
 	} elseif (isset($_POST['k'])) {
-		#**User confirmed viewing the secret**
 		try {
-			$secret = retrieve_secret($_POST['k']);
-			$message = htmlentities($secret);
-			require_once('html/view_secret.php');
-		} catch (Exception $e) {
-			$error_message = $e->getMessage();
-			require_once('html/error.php');
-		}
+			display_secret(); # user confirmed viewing the secret
+		} catch (Exception $e) { display_error($e); }
 
 	} elseif (isset($_POST['submit'])) {
-		#**User just submitted a secret. Show them the generated URL**
 		try {
-			$incoming_text = $_POST['secret'];
-
-			if ( strlen($incoming_text) > $settings['max_secret_length'] ) {
-				throw new exception("Input length too long");
-			}
-
-			$k = store_secret($incoming_text);
-
-			if ($settings['return_full_url'] == true) {
-				# construct retrieval url
-				if ( isset($_SERVER['REQUEST_SCHEME']) ) {
-					$scheme = $_SERVER['REQUEST_SCHEME'] . '://'; # https://
-				} else {
-					$scheme = 'https://';
-				}
-				$hostname = $_SERVER['HTTP_HOST']; # my.flashpaper.io
-				$path = strtok($_SERVER['REQUEST_URI'], '?'); # strip any GET vars from url (like ?t=bla)
-				$path = str_replace("index.php","",$path); # remove index.php from path if it's there
-				$path = preg_replace('/(\/+)/','/',$path); # strip any duplicate /'s from path
-				$path = rtrim($path, '/') . '/'; # make sure path ends with /
-				$args = "?k=${k}"; # /?k=a1b2c3d4...
-
-				# display URL to user
-				$message = "${scheme}${hostname}${path}${args}";
-				require_once('html/view_code.php');
-			} else {
-				# display 'k' value of URL to user
-				$message = $k;
-				require_once('html/view_code.php');
-			}
-		} catch (Exception $e) {
-			$error_message = $e->getMessage();
-			require_once('html/error.php');
-		}
+			display_secret_code(); # secret submitted. display url/code
+		} catch (Exception $e) { display_error($e); }
 
 	} else {
-		#**User is loading the main page**
+		try {
+			display_form(); # display main page
+		} catch (Exception $e) { display_error($e); }
+	}
 
-		#Get template from URL (if any)
-		$template_text = "";
+	require_once('html/footer.php'); # display footer
 
-		if (isset($_GET['t']) && $_GET['t'] != "") {
-			$template_text = file_get_contents('templates/' . basename($_GET['t'] . '.txt'));
+	function display_form() {
+		global $settings;
+
+		$template_text = '';
+		if ( isset($_GET['t']) && $_GET['t'] != "" ) {
+			$safe_path = 'templates/' . basename($_GET['t']) . '.txt';
+			if ( file_exists($safe_path) ) { $template_text = file_get_contents($safe_path); }
 		}
-
 		require_once('html/submit_secret.php');
 	}
 
-	require_once('html/footer.php');
+	function confirm_display_secret() {
+		global $settings;
+
+		require_once('html/confirm.php');
+	}
+
+	function display_secret() {
+		global $settings;
+
+		$secret = retrieve_secret($_POST['k']);
+		$message = htmlentities($secret);
+		require_once('html/view_secret.php');
+	}
+
+	function display_secret_code() {
+		global $settings;
+
+		# verify secret length isnt too long
+		if ( strlen($incoming_text) > $settings['max_secret_length'] ) { throw new exception("Input length too long"); }
+
+		$message = store_secret($_POST['secret']);
+
+		if ($settings['return_full_url'] == true) {
+			$message = build_url($message);
+		}
+		require_once('html/view_code.php');
+	}
+
+	function build_url($k) {
+		$scheme = (isset($_SERVER['REQUEST_SCHEME'])) ? $_SERVER['REQUEST_SCHEME'] . '://' : 'https://';
+		$hostname = $_SERVER['HTTP_HOST']; # my.flashpaper.io
+		$path = strtok($_SERVER['REQUEST_URI'], '?'); # strip any GET vars from url (like ?t=bla)
+		$path = str_replace("index.php","",$path); # remove index.php from path if it's there
+		$path = preg_replace('/(\/+)/','/',$path); # strip any duplicate /'s from path
+		$path = rtrim($path, '/') . '/'; # make sure path ends with /
+		$args = "?k=${k}"; # /?k=a1b2c3d4...
+		return "${scheme}${hostname}${path}${args}";
+
+	}
+
+	function display_error($exception) {
+		$error_message = $exception->getMessage();
+		require_once('html/error.php');
+	}
+
 ?>
