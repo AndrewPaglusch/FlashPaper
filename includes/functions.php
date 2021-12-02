79d37404d218a403026b27919a3351f25a88494a
AndrewPaglusch <AndrewPaglusch@users.noreply.github.com>
2021-12-02 18:46:42 +0000

    fix indentation

--- includes/functions.php
+++ includes/functions.php
@@ -119,11 +119,11 @@
 
 	function crypto_rand_string($strLen) {
 		# random_int() generates cryptographically secure pseudo-random integers
-	        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	        $key = '';
-	        for ($i = 0; $i < $strLen; ++$i) {
-	                $key .= $chars[random_int(0, strlen($chars) -1)];
-	        }
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$key = '';
+		for ($i = 0; $i < $strLen; ++$i) {
+			$key .= $chars[random_int(0, strlen($chars) -1)];
+		}
 		return $key;
 	}
 
