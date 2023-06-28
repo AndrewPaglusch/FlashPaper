69fce1c0bdaec66c69ec7b4f8a0c7d275ae3b00d
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-28 14:05:56 -0500

    ta border

--- html/footer.php
+++ html/footer.php
@@ -2,7 +2,6 @@
 		<footer style="width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
 			<div class="container">
 				<span class="text-muted small">Source code for GhostSecret is freely available at <a href="https://github.com/bleach86/FlashPaper">Github.com</a></span>
-				<br>
 				<span class="text-muted small">Source code for FlashPaper is freely available at <a href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
 			</div>
 		</footer>
