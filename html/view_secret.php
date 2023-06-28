e40656a6d06a81368d4e35835269e34d1835e56e
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-28 18:49:38 -0500

    ta border

--- html/view_secret.php
+++ html/view_secret.php
@@ -1,17 +1,19 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
-		<div id="form-div">
-			<fieldset>
-				<div>
-					<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 30px; font-weight: bold;"><?php echo $settings['messages']['view_secret_header'] ?></label>
-					<div style="font-style: italic">
-						<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 15px; font-weight: bold;"><?php echo $settings['messages']['view_secret_subheader'] ?></label>
+		<div class="centered-div">
+			<div id="form-div">
+				<fieldset>
+					<div>
+						<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 30px; font-weight: bold;"><?php echo $settings['messages']['view_secret_header'] ?></label>
+						<div style="font-style: italic">
+							<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 15px; font-weight: bold;"><?php echo $settings['messages']['view_secret_subheader'] ?></label>
+						</div>
+						<div style="margin-top:10px">
+							<textarea readonly id="copy" class="form-control" name="secret" rows="8" style="resize: vertical; margin-bottom: 15px"><?php echo $message ?></textarea>
+						</div>
+						<div class="col">
+							<button class="btn btn-primary" type="button" onclick="copyText()">Copy</button>
+						</div>
 					</div>
-					<div style="margin-top:10px">
-						<textarea readonly id="copy" class="form-control" name="secret" rows="8" style="resize: vertical; margin-bottom: 15px"><?php echo $message ?></textarea>
-					</div>
-					<div class="col">
-						<button class="btn btn-primary" type="button" onclick="copyText()">Copy</button>
-					</div>
-				</div>
-			</fieldset>
+				</fieldset>
+			</div>
 		</div>
