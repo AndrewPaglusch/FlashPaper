41e8b89ebdd920a7a21960a9f6dab7e72d9df432
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-28 18:03:23 -0500

    ta border

--- html/footer.php
+++ html/footer.php
@@ -1,7 +1,7 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
 		<footer style="width: 100%; color:#7b7b7b; height: 30px; line-height: 30px; background-color: #232728;" class="fixed-bottom">
 			<div class="container centered-div">
-				<span class="text-muted small">Source code for FlashPaper is freely available at <a style="color:#aeff00" href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
+				<span class="text-muted small">Source code is freely available at <a style="color:#aeff00" href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
 			</div>
 		</footer>
 	</body>
