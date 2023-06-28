74f67a5f2742b81208eaa70213aa9fbf617fdd79
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-28 14:02:09 -0500

    ta border

--- html/footer.php
+++ html/footer.php
@@ -1,7 +1,8 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
 		<footer style="width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
 			<div class="container">
-				<span class="text-muted small">GhostSecret is a fork of FlashPaper, and it's source is freely available at <a href="https://github.com/bleach86/FlashPaper">Github.com</a></span>
+				<span class="text-muted small">Source code for GhostSecret is freely available at <a href="https://github.com/bleach86/FlashPaper">Github.com</a></span>
+				<br>
 				<span class="text-muted small">Source code for FlashPaper is freely available at <a href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
 			</div>
 		</footer>
