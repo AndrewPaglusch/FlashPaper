113466260dbc92411d4a22b96fdc0910d68f7d08
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-28 12:54:28 -0500

    ta border

--- html/header.php
+++ html/header.php
@@ -43,7 +43,7 @@
 				height: 200px;
 				padding: 12px 20px;
 				box-sizing: border-box;
-				border: 2px solid #ccc;
+				border: 2px solid #aeff00;
 				border-radius: 4px;
 				background-color: #f8f8f8;
 				resize: none;
