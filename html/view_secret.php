c7f89311dcd2a6cf0fa0e07a268a8ebc4eecfbbd
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-29 07:38:24 -0500

    ta border

--- html/view_secret.php
+++ html/view_secret.php
@@ -3,9 +3,9 @@
 			<div id="form-div">
 				<fieldset>
 					<div>
-						<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 30px; font-weight: bold;"><?php echo $settings['messages']['view_secret_header'] ?></label>
+						<label style="font-family: 'Proxima Nova', arial, serif; color:#fafafa; line-height: 1.25; margin: 0 0 10px; font-size: 30px; font-weight: bold;"><?php echo $settings['messages']['view_secret_header'] ?></label>
 						<div style="font-style: italic">
-							<label style="font-family: 'Proxima Nova', arial, serif; line-height: 1.25; margin: 0 0 10px; font-size: 15px; font-weight: bold;"><?php echo $settings['messages']['view_secret_subheader'] ?></label>
+							<label style="font-family: 'Proxima Nova', arial, serif; color:#fafafa; line-height: 1.25; margin: 0 0 10px; font-size: 15px; font-weight: bold;"><?php echo $settings['messages']['view_secret_subheader'] ?></label>
 						</div>
 						<div style="margin-top:10px">
 							<textarea readonly id="copy" class="form-control" name="secret" rows="8" style="resize: vertical; margin-bottom: 15px"><?php echo $message ?></textarea>
