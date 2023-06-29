a13bafbba29c88fe0789012f751c62e8d770896e
bleach86 <14421739+bleach86@users.noreply.github.com>
2023-06-29 06:10:47 -0500

    ta border

--- html/submit_secret.php
+++ html/submit_secret.php
@@ -37,6 +37,9 @@
 								</div>
 							</div>
 						</div>
+						<div class="slidecontainer">
+							<input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+						</div>
 					</fieldset>
 				</form>
 			</div>
