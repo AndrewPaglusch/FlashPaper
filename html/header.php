dc5e3d82e101ddb89e53fba4add856fc427e408f
Barry Smith <barry.smithjr@gmail.com>
2024-04-24 16:30:56 -0500

    Revert "move flashpaper JS to flashpaper.js"
    This reverts commit 1598ccd27c944dcba370e1eb91e678198ad53689.

--- html/header.php
+++ html/header.php
@@ -72,10 +72,19 @@
 				width: 100% !important;
 			}
 		</style>
-		<script src="./js/flashpaper.js" defer>
 		<script src="./js/color-toggle.js" defer>
 	</head>
 	<body onUnload="document.getElementById('secret').value = ''">
+	<script>
+			if (location.protocol != 'https:') {
+				document.write('<div style="padding-top: 1%" class="container"><div class="alert alert-danger"><strong>Danger!</strong> This site is not being accessed over an encrypted connection. Do NOT input any sensitive information!</div></div>');
+			}
+			function copyText() {
+				var textToCopy = document.getElementById("copy");
+				textToCopy.select();
+				document.execCommand("copy");
+			}
+		</script>
 		<header>
 			<nav class="navbar navbar-expand-lg bg-body-tertiary">
 		  		<div class="container-fluid">
