f3fc6cee0b3be2a9d5695780a779f4f5cdad2929
Barry Smith <barry.smithjr@gmail.com>
2024-04-24 14:20:28 -0500

    fix pathing for script src

--- html/footer.php
+++ html/footer.php
@@ -4,6 +4,6 @@
 				<span class="text-muted small">Source code for FlashPaper is freely available at <a href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
 			</div>
 		</footer>
-		<script src=".js/color-toggle.js">
+		<script src="./js/color-toggle.js">
 	</body>
 </html>
