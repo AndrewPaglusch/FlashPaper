bc9038f4dc515981343765f5bfd79f045fbfc8f4
Barry Smith <barry.smithjr@gmail.com>
2024-04-24 15:57:49 -0500

    localize fontawesome solid and update footer bg color

--- html/footer.php
+++ html/footer.php
@@ -1,5 +1,5 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
-		<footer style="width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
+		<footer style="width: 100%; height: 30px; line-height: 30px;" class="fixed-bottom bg-secondary">
 			<div class="container">
 				<span class="text-muted small">Source code for FlashPaper is freely available at <a href="https://github.com/AndrewPaglusch/FlashPaper">Github.com</a></span>
 			</div>
