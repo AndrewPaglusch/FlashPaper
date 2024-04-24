9cebc3bcf8385caef0c517e83c8fe7cb5b8f5bfd
Barry Smith <barry.smithjr@gmail.com>
2024-04-24 15:34:21 -0500

    include just the solid icons, move js loading to head and defer

--- html/header.php
+++ html/header.php
@@ -12,6 +12,7 @@
 		<!-- Meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="./css/solid.min.css" >
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="./css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 		<style>
@@ -69,6 +70,7 @@
 				width: 100% !important;
 			}
 		</style>
+		<script src="./js/color-toggle.js" defer>
 	</head>
 	<body onUnload="document.getElementById('secret').value = ''">
 		<header>
